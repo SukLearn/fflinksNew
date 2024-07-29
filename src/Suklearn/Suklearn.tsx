458d5a4cae0a1efc1a6e82a7be1d76553c8cada9
Luka <luka.tarkhnishvili2002@gmail.com>
2024-07-29 15:48:34 +0400

    "Added CSS styles and refactored SukLearn component to include cursor functionality and styling"

--- src/Suklearn/Suklearn.tsx
+++ src/Suklearn/Suklearn.tsx
@@ -1,16 +1,46 @@
 import { Link } from "react-router-dom";
-
+import styles from "./SukLearn.module.css";
+import { useEffect, useRef } from "react";
 export default function SukLearn() {
+  const cursorRef = useRef(null);
+  useEffect(() => {
+    const cursor = cursorRef.current;
+    if (cursor) {
+      console.log("element ", cursor);
+    }
+  }, []);
+
+  const handleMouseMove = (event: React.MouseEvent) => {
+    // console.log("mouse pos: ", event.clientX, event.clientY);
+    const leftPos = event.clientX;
+    const topPost = event.clientY;
+  };
+
   return (
     <>
-      <h1>test</h1>
-      <Link to="/fflinks">fflinks</Link>
-      <br />
-      <Link to="/projects">Projects</Link>
-      <br />
-      <Link to="/contact">contact</Link>
-      <br />
-      <Link to="/honors">honors</Link>
+      <div onMouseMove={handleMouseMove}>
+        <div className={styles.cursor} ref={cursorRef}>
+          <h1>test</h1>
+        </div>
+        {/* <div className={styles.rounded}>
+          <div className={styles.pointed}></div>
+        </div> */}
+        <Link className={styles.links} to="/fflinks">
+          fflinks
+        </Link>
+        <br />
+        <Link className={styles.links} to="/projects">
+          Projects
+        </Link>
+        <br />
+        <Link className={styles.links} to="/contact">
+          contact
+        </Link>
+        <br />
+        <Link className={styles.links} to="/honors">
+          honors
+        </Link>
+      </div>
     </>
   );
 }
