b6db5bc05edc84fc11b04674101f31bbc8d648f8
Luka <luka.tarkhnishvili2002@gmail.com>
2024-08-02 23:27:40 +0400

    "Updated Content component CSS and TSX files with new styles and layout"

--- src/Components/Content/Content.tsx
+++ src/Components/Content/Content.tsx
@@ -3,17 +3,20 @@ export default function Content() {
   return (
     <>
       <div className={styles.content}>
-        <p>Hi my name is</p>
-        <p>Luka Tarkhnishvili</p>
-        <p>I build things for web.</p>
+        <div className={styles.greetingContainer}>
+          <p id={styles.greeting}>&lt;p&gt; Hi my name is &lt;p&gt;</p>
+        </div>
+        <p id={styles.author}>&lt; Luka Tarkhnishvili &gt;</p>
+        <p id={styles.main}>&lt; I build things for web &gt;</p>
 
-        {/* small */}
-        <p>
+        <p id={styles.contentText}>
           I am a beginner in web development, with a strong passion for
           developing and coding logic. Currently, I am focused on honing my
           skills and expanding my knowledge in this field.
         </p>
-        <button>Check Out my Projects</button>
+        <button type="button" className={styles.checkout}>
+          Check Out my Projects
+        </button>
       </div>
     </>
   );
