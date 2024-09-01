328164560ca76d453b8912769d50e04eafc37302
Luka <luka.tarkhnishvili2002@gmail.com>
2024-09-01 18:06:56 +0400

    "Updated index.html title, added background image and styles to Watch.module.css, refactored Watch.tsx component with new functionality and layout"

--- src/Components/Projects/Watch/Watch.tsx
+++ src/Components/Projects/Watch/Watch.tsx
@@ -1,58 +1,143 @@
 import styles from "./Watch.module.css";
 import "../../../index.css";
 import { useEffect, useState } from "react";
-
-export default function Watch() {
+import SukModal from "../../hModal/sukModal";
+// showing code modal and start stop
+const codeTsx = `export default function Watch() {
   const [time, setTime] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
   const [showCss, setShowCss] = useState(false);
   const [copy, setCopy] = useState(false);
-  const codeTsx = `import styles from "./Watch.module.css";
-import "../../../index.css";
-import { useEffect, useState } from "react";
-
-export default function Watch() {
-  const [time, setTime] = useState(0);
-  const [isRunning, setIsRunning] = useState(false);
+  const [activeStart, setActiveStart] = useState(false);
+  const [activeReset, setActiveReset] = useState(false);
 
   useEffect(() => {
     let interval: number;
     if (isRunning) {
       interval = setInterval(() => setTime((prevTime) => prevTime + 10), 10);
     }
-    return () => {
-      clearInterval(interval);
-    };
+    return () => clearInterval(interval);
   }, [isRunning]);
 
   const startStop = () => setIsRunning(!isRunning);
-
   const milliseconds = time % 1000;
   const seconds = Math.floor((time / 1000) % 60);
   const minutes = Math.floor((time / 60000) % 60);
   const hours = Math.floor(time / 3600000);
 
+  const handleCss = () => setShowCss(true);
+  const handleTsx = () => setShowCss(false);
+  const handleCopy = () => {
+    showCss ? codeCss : codeTsx;
+    const text = navigator.clipboard.writeText(showCss ? codeCss : codeTsx);
+    setCopy(true);
+
+    setInterval(() => setCopy(false), 2000);
+    console.log({ text });
+    console.clear();
+  };
+
   const formatTime = (time: number, digits: number = 2) =>
     time.toString().padStart(digits, "0");
+  const formatMilliseconds = (time: number) =>
+    Math.floor(time / 10)
+      .toString()
+      .padStart(2, "0");
+  const reset = () => {
+    setTime(0);
+    setIsRunning(false);
+  };
+  const handleKeydown = (e: KeyboardEvent) => {
+    if (e.key == " " || e.key == "Enter") {
+      startStop();
+      setActiveStart(true);
+    } else if (e.key == "r") {
+      reset();
+      setActiveReset(true);
+    }
+  };
+  const handleKeyUp = (e: KeyboardEvent) => {
+    if (e.key == " " || e.key == "Enter") setActiveStart(false);
+    else if (e.key == "r") setActiveReset(false);
+  };
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeydown);
+    window.addEventListener("keyup", handleKeyUp);
 
+    return () => {
+      window.removeEventListener("keydown", handleKeydown);
+      window.removeEventListener("keyup", handleKeyUp);
+    };
+  });
   return (
-    <div className={styles.wrapper }>
-      <p className="text-6xl p-3 mt-52 bg-slate-700  rounded-xl">
-        <span className="">{formatTime(hours)}</span>:
-        <span className="">{formatTime(minutes)}</span>:{formatTime(seconds)}:
-        {formatTime(milliseconds)}
+    <div className={\`\${styles.wrapper} grid justify-items-stretch\`}>
+      <p className="text-6xl p-3 mt-20 bg-slate-700  rounded-xl">
+        {formatTime(hours)}:{formatTime(minutes)}:{formatTime(seconds)}:
+        {formatMilliseconds(milliseconds)}
       </p>
+      <div className={\`\${styles.buttons} grid grid-cols-2 gap-10\`}>
+        <button
+          onClick={startStop}
+          className={\`\${styles.btn} \${styles.btnWhite} \${styles.btnAnimate} \${
+            activeStart ? \`\${styles.btnActive} \${styles.btnWhiteActive}\` : ""
+          }\`}
+        >
+          {!isRunning ? "Start" : "Stop"}
+        </button>
+        <button
+          onClick={reset}
+          className={\`\${styles.btn} \${styles.btnWhite} \${styles.btnAnimate} \${
+            activeReset ? \`\${styles.btnActive} \${styles.btnWhiteActive}\` : ""
+          }\`}
+        >
+          Reset
+        </button>
+      </div>
 
-      <button
-        onClick={startStop}
-      >
-        {!isRunning ? "Start" : "Stop"}
-      </button>
+      <section className="grid grid-cols-3  gap-x-[185px] mt-4 border w-[800px] ">
+        <div>
+          <button
+            onClick={handleTsx}
+            className="w-36 py-2 text-xl font-bold border-2 border-white	hover:bg-gray-500 "
+          >
+            Tsx
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={handleCss}
+            className="w-36 py-2 text-xl font-bold border-2 border-white	hover:bg-gray-500 "
+          >
+            css
+          </button>
+        </div>
+        <div className="">
+          <button
+            className={\`\${styles.copy} w-36 py-2 text-xl font-bold border-2 border-white	hover:bg-gray-500\`}
+            onClick={handleCopy}
+          >
+            {copy ? <span>&#9989;</span> : "Copy"}
+          </button>
+        </div>
+
+        <div className={styles.code}>
+          <pre className="text-lg">{showCss ? codeCss : codeTsx}</pre>
+        </div>
+      </section>
+      <SukModal
+        title="stopWatch"
+        text={text}
+        problems={problems}
+        front="React Vite, Typescript"
+      />
     </div>
   );
-}
-`;
-  const codeCss = `body {
+}`;
+const codeCss = `body {
+  background-image: url(https://images.pexels.com/photos/5624978/pexels-photo-5624978.jpeg);
+  background-repeat: no-repeat;
+  /* background-size: cover; */
+  background-attachment: fixed;
   padding: 0;
   margin: 0;
   box-sizing: border-box;
@@ -68,7 +153,7 @@ export default function Watch() {
 }
 
 .btn {
-  margin: 50px 0 0 0;
+  margin: 30px 0 30px 0;
   font-size: 2rem;
   padding: 35px 80px;
   display: inline-block;
@@ -128,6 +213,32 @@ export default function Watch() {
   animation-fill-mode: backwards;
 }
 
+.btnActive {
+  transform: translateY(-1px);
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+  content: "";
+  display: inline-block;
+  display: flex;
+  z-index: -1;
+  transition: all 0.4s;
+  -webkit-transition: all 0.4s;
+  -moz-transition: all 0.4s;
+  -ms-transition: all 0.4s;
+  -o-transition: all 0.4s;
+}
+
+.btnActive::after {
+  opacity: 0;
+  transform: scaleX(1.4) scaleY(1.6);
+  -webkit-transform: scaleX(1.4) scaleY(1.6);
+  -moz-transform: scaleX(1.4) scaleY(1.6);
+  -ms-transform: scaleX(1.4) scaleY(1.6);
+  -o-transform: scaleX(1.4) scaleY(1.6);
+}
+.btnWhiteActive {
+  background-color: #fff;
+  color: #777;
+}
 @-webkit-keyframes moveInBottom {
   0% {
     opacity: 0;
@@ -152,33 +263,21 @@ export default function Watch() {
 
 .code {
   overflow: auto;
-  height: 500px;
-}
-.codeSection {
-  margin: 50px 0 0 0;
-  border: 1px solid white;
-}
-.codeButton:hover {
-  background-color: white;
-  color: black;
+  height: 540px;
+  width: 800px;
 }
 `;
-  useEffect(() => {
-    let interval: number;
-    if (isRunning) {
-      interval = setInterval(() => setTime((prevTime) => prevTime + 10), 10);
-    }
-    return () => {
-      clearInterval(interval);
-    };
-  }, [isRunning]);
-
-  const startStop = () => setIsRunning(!isRunning);
-
-  const milliseconds = time % 1000;
-  const seconds = Math.floor((time / 1000) % 60);
-  const minutes = Math.floor((time / 60000) % 60);
-  const hours = Math.floor(time / 3600000);
+const text = `The stopwatch is a simple yet functional time-tracking tool designed to measure the duration of an event or task. It features a user-friendly interface that allows users to start, stop, and reset the timer with ease. The stopwatch displays the elapsed time in a clear and precise format, showing hours, minutes, seconds, and milliseconds.`;
+const problems = `The problem that I encountered was with setInterval. the function would not count if it wasn't in active tab, also I needed to adjust how it counts(intervals) because it worked differently than it should bu logic. the time wasn't matching phone's stopWatch. Both Problems solved via Date.now() function`;
+// const newTsx = ``;
+export default function Watch() {
+  const [showCss, setShowCss] = useState(false);
+  const [copy, setCopy] = useState(false);
+  const [activeStart, setActiveStart] = useState(false);
+  const [activeReset, setActiveReset] = useState(false);
+  const [start, setStart] = useState(false);
+  const [time, setTime] = useState(0);
+  const [startTime, setStartTime] = useState<number | null>(0);
 
   const handleCss = () => setShowCss(true);
   const handleTsx = () => setShowCss(false);
@@ -192,36 +291,104 @@ export default function Watch() {
     console.clear();
   };
 
+  const Start = () => {
+    if (!start && startTime === 0) {
+      setStartTime(Date.now());
+    }
+    setStart(!start);
+  };
+
+  const Reset = () => {
+    setTime(0);
+    setStartTime(null);
+    setStart(false);
+  };
+
+  useEffect(() => {
+    let interval: number;
+    if (start && startTime !== null) {
+      interval = setInterval(() => {
+        setTime(Date.now() - startTime);
+      }, 10);
+    }
+    return () => clearInterval(interval);
+  }, [start, startTime]);
+
+  const Milliseconds = time % 1000;
+  const Seconds = Math.floor((time / 1000) % 60);
+  const Minutes = Math.floor((time / 60000) % 60);
+  const Hours = Math.floor(time / 3600000);
+
   const formatTime = (time: number, digits: number = 2) =>
     time.toString().padStart(digits, "0");
   const formatMilliseconds = (time: number) =>
     Math.floor(time / 10)
       .toString()
       .padStart(2, "0");
-  const reset = () => {
-    setTime(0);
-    setIsRunning(false);
+
+  const handleKeydown = (e: KeyboardEvent) => {
+    if (e.key == " " || e.key == "Enter") {
+      Start();
+      setActiveStart(true);
+    } else if (e.key == "r") {
+      Reset();
+      setActiveReset(true);
+    }
   };
+  const handleKeyUp = (e: KeyboardEvent) => {
+    if (e.key == " " || e.key == "Enter") setActiveStart(false);
+    else if (e.key == "r") setActiveReset(false);
+  };
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeydown);
+    window.addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeydown);
+      window.removeEventListener("keyup", handleKeyUp);
+    };
+  });
   return (
     <div className={`${styles.wrapper} grid justify-items-stretch`}>
       <p className="text-6xl p-3 mt-20 bg-slate-700  rounded-xl">
-        {formatTime(hours)}:{formatTime(minutes)}:{formatTime(seconds)}:
-        {formatMilliseconds(milliseconds)}
+        {formatTime(Hours)}:{formatTime(Minutes)}:{formatTime(Seconds)}:
+        {formatMilliseconds(Milliseconds)}
       </p>
-      <button onClick={reset}>Reset</button>
-      <button
-        onClick={startStop}
-        className={`${styles.btn} ${styles.btnWhite} ${styles.btnAnimate}`}
-      >
-        {!isRunning ? "Start" : "Stop"}
-      </button>
-      <section className="grid grid-cols-3  gap-x-[185px] mt-4 border w-[800px]">
+
+      <div className={`${styles.buttons} grid grid-cols-2 gap-10`}>
+        <button
+          onClick={Start}
+          className={`${styles.btn} ${styles.btnWhite} ${styles.btnAnimate} ${
+            activeStart ? `${styles.btnActive} ${styles.btnWhiteActive}` : ""
+          }`}
+        >
+          {!start ? "Start" : "Stop"}
+        </button>
+        <button
+          onClick={Reset}
+          className={`${styles.btn} ${styles.btnWhite} ${styles.btnAnimate} ${
+            activeReset ? `${styles.btnActive} ${styles.btnWhiteActive}` : ""
+          }`}
+        >
+          Reset
+        </button>
+      </div>
+      {/*  gap-x-[185px] */}
+      <section className="grid grid-cols-4  mt-4 border w-[800px] ">
         <div>
           <button
             onClick={handleTsx}
             className="w-36 py-2 text-xl font-bold border-2 border-white	hover:bg-gray-500 "
           >
             Tsx
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={handleTsx}
+            className="w-36 py-2 text-xl font-bold border-2 border-white	hover:bg-gray-500 "
+          >
+            Tsx(Old)
           </button>
         </div>
         <div>
@@ -245,6 +412,12 @@ export default function Watch() {
           <pre className="text-lg">{showCss ? codeCss : codeTsx}</pre>
         </div>
       </section>
+      <SukModal
+        title="stopWatch"
+        text={text}
+        problems={problems}
+        front="React Vite, Typescript"
+      />
     </div>
   );
 }
---
+++ src/Components/hModal/sukModal.tsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react";
+import Modal from "react-modal";
+import styles from "./sukModal.module.css";
+interface ModalProps {
+  title: string;
+  text: string;
+  problems: string;
+  front: string;
+  back?: string;
+}
+const SukModal: React.FC<ModalProps> = ({
+  title,
+  text,
+  problems,
+  front,
+  back = "N/A",
+}) => {
+  const [help, setHelp] = useState(false);
+  const [keySequence, setKeySequence] = useState<string[]>([]);
+
+  function closeModal() {
+    setHelp(false);
+    console.log(keySequence);
+  }
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      const target = event.target as HTMLElement;
+      if (
+        target.tagName.toLowerCase() === "input" ||
+        target.tagName.toLowerCase() === "textarea"
+      ) {
+        return;
+      }
+      setKeySequence((prevSequence) => {
+        const newSequence = [...prevSequence, event.key.toLowerCase()];
+        const sequenceString = newSequence.join("");
+        if (sequenceString.includes("fflinks")) {
+          window.open("https://fflinks.net");
+          return [];
+        } else if (sequenceString.includes("suk")) {
+          window.location.replace("https://suklearn.com");
+        }
+        return newSequence;
+      });
+      if (event.key === "h" || event.key === "H") {
+        setHelp(true);
+      }
+    };
+    window.addEventListener("keyup", handleKeyDown);
+    return () => {
+      window.removeEventListener("keyup", handleKeyDown);
+    };
+  });
+  return (
+    <>
+      <Modal
+        isOpen={help}
+        onRequestClose={closeModal}
+        contentLabel="Content"
+        ariaHideApp={false}
+      >
+        <div className={styles.modal}>
+          <h1 className={styles.title}>{title}</h1>
+
+          <div className={styles.content}>
+            <p>{text}</p>
+            <p>
+              technologies used in this project are: <br />
+              front:{front} <br />
+              back:{back}
+            </p>
+            <br />
+            <p>{problems}</p>
+          </div>
+        </div>
+      </Modal>
+    </>
+  );
+};
+
+export default SukModal;
